a4523585bca5d265c7d2c9538f54a38d2b131274
Ananda Rauf <anandarauf02@gmail.com>
2024-01-04 12:53:39 +0700

    Menambahkan folder dan file jsonsql

---
+++ file Json to SQL/jsonsqlDes23.py
@@ -0,0 +1,300 @@
+import jpype
+import asposecells  
+  
+jpype.startJVM() 
+from asposecells.api import Workbook
+print("==========================================================================================\n")
+namaaplikasi = "Tools Json SQL\n"
+versi = "Version 1.0\n"
+project= "Pustadin Kemenhub"
+devby = "Ananda Rauf Maududi\n"
+devdate = "Project dimulai pada 04 Januari 2024\n"
+print(namaaplikasi)
+print(versi)
+print("Developed by:",devby)
+print(devdate)
+print("==========================================================================================\n")
+
+def MenuToolsJsonSQL():
+    print("Pilih nomor yang ada didalam menu:")
+    print
+    print("1.01 Desember 2023")
+    print("2.02 Desember 2023")
+    print("3.03 Desember 2023")
+    print("4.04 Desember 2023")
+    print("5.05 Desember 2023")
+    print("6.06 Desember 2023")
+    print("7.07 Desember 2023")
+    print("8.08 Desember 2023")
+    print("9.09 Desember 2023")
+    print("10.10 Desember 2023")
+    print("11.11 Desember 2023")
+    print("12.12 Desember 2023")
+    print("13.13 Desember 2023")
+    print("14.14 Desember 2023")
+    print("14.15 Desember 2023")
+    print("16.16 Desember 2023")
+    print("17.17 Desember 2023")
+    print("18.18 Desember 2023")
+    print("19.19 Desember 2023")
+    print("20.20 Desember 2023")
+    print("21.21 Desember 2023")
+    print("22.22 Desember 2023")
+    print("23.23 Desember 2023")
+    print("24.24 Desember 2023")
+    print("25.25 Desember 2023")
+    print("26.26 Desember 2023")
+    print("27.27 Desember 2023")
+    print("28.28 Desember 2023")
+    print("29.29 Desember 2023")
+    print("30.30 Desember 2023")
+    
+def Des01():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-12-01.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-12-01.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+def Des02():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-12-02.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-12-02.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+def Des03():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-12-03.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-12-03.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Des04():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-12-04.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-12-04.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+        
+def Des05():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-12-05.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-12-05.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Des06():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-12-06.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-12-06.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Des07():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-12-07.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-12-07.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Des08():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-12-08.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-12-08.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Des09():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-12-09.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-12-09.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Des10():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-12-10.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-12-10.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Des11():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-12-11.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-12-12.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Des12():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-12-12.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-12-12.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Des13():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-12-13.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-12-13.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Des14():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-12-14.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-12-14.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Des15():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-12-15.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-12-15.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Des16():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-12-16.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-12-16.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Des17():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-12-17.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-12-17.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Des18():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-12-18.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-12-18.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Des19():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-12-19.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-12-19.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Des20():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-12-20.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-12-20.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Des21():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-12-21.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-12-21.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Des22():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-12-22.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-12-22.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Des23():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-12-23.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-12-23.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Des24():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-12-24.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-12-24.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Des25():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-12-25.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-12-25.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Des26():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-12-26.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-12-26.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Des27():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-12-27.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-12-27.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Des28():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-12-28.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-12-28.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Des29():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-12-29.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-12-29.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Des30():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-12-30.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-12-30.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+
+
+MenuToolsJsonSQL()
+
+pilih = int(input("Masukan nomor pilihan dalam menu:"))
+if pilih == 1:
+         Des01()
+elif pilih==2:
+        Des02()
+elif pilih==3:
+        Des03()
+elif pilih==4:
+        Des04()
+elif pilih==5:
+        Des05()
+elif pilih==6:
+        Des06()
+elif pilih==7:
+        Des07()
+elif pilih==8:
+        Des08()
+elif pilih==9:
+        Des09()
+elif pilih==10:
+        Des10()
+elif pilih==11:
+        Des11()        
+elif pilih==12:
+        Des12()
+
+elif pilih==13:
+        Des13()
+elif pilih==14:
+        Des14()
+elif pilih==15:
+        Des15()
+elif pilih==16:
+        Des16()
+elif pilih==17:
+        Des17()
+elif pilih==18:
+        Des18()
+elif pilih==19:
+        Des20()
+elif pilih==20:
+        Des21()
+elif pilih==21:
+        Des22()
+elif pilih==22:
+        Des23()
+elif pilih==23:
+        Des24()
+elif pilih==24:
+        Des25()
+elif pilih==25:
+        Des26()
+elif pilih==26:
+        Des27()
+elif pilih==27:
+        Des28()
+elif pilih==28:
+        Des29()
+elif pilih==29:
+        Des29()
+elif pilih==30:
+        Des30()
+
+        
+else:
+    print("Nomor menu tidak tersedia!")
+    exit
---
+++ file Json to SQL/jsonsqlNov23.py
@@ -0,0 +1,300 @@
+import jpype
+import asposecells  
+  
+jpype.startJVM() 
+from asposecells.api import Workbook
+print("==========================================================================================\n")
+namaaplikasi = "Tools Json SQL\n"
+versi = "Version 1.0\n"
+project= "Pustadin Kemenhub"
+devby = "Ananda Rauf Maududi\n"
+devdate = "Project dimulai pada 04 Januari 2024\n"
+print(namaaplikasi)
+print(versi)
+print("Developed by:",devby)
+print(devdate)
+print("==========================================================================================\n")
+
+def MenuToolsJsonSQL():
+    print("Pilih nomor yang ada didalam menu:")
+    print
+    print("1.01 November 2023")
+    print("2.02 November 2023")
+    print("3.03 November 2023")
+    print("4.04 November 2023")
+    print("5.05 November 2023")
+    print("6.06 November 2023")
+    print("7.07 November 2023")
+    print("8.08 November 2023")
+    print("9.09 November 2023")
+    print("10.10 November 2023")
+    print("11.11 November 2023")
+    print("12.12 November 2023")
+    print("13.13 November 2023")
+    print("14.14 November 2023")
+    print("14.15 November 2023")
+    print("16.16 November 2023")
+    print("17.17 November 2023")
+    print("18.18 November 2023")
+    print("19.19 November 2023")
+    print("20.20 November 2023")
+    print("21.21 November 2023")
+    print("22.22 November 2023")
+    print("23.23 November 2023")
+    print("24.24 November 2023")
+    print("25.25 November 2023")
+    print("26.26 November 2023")
+    print("27.27 November 2023")
+    print("28.28 November 2023")
+    print("29.29 November 2023")
+    print("30.30 November 2023")
+    
+def Nov01():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-11-01.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-11-01.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+def Nov02():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-11-02.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-11-02.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+def Nov03():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-11-03.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-11-03.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Nov04():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-11-04.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-11-04.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+        
+def Nov05():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-11-05.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-11-05.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Nov06():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-11-06.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-11-06.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Nov07():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-11-07.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-11-07.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Nov08():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-11-08.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-11-08.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Nov09():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-11-09.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-11-09.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Nov10():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-11-10.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-11-10.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Nov11():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-11-11.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-11-11.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Nov12():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-11-12.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-11-12.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Nov13():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-11-13.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-11-13.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Nov14():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-11-14.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-11-14.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Nov15():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-11-15.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-11-15.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Nov16():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-11-16.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-11-16.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Nov17():
+        workbook = Workbook("jasamargakodegerbang2gerbang31_2023-11-17.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-11-17.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Nov18():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-11-18.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-11-18.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Nov19():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-11-19.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-11-19.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Nov20():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-11-20.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-11-20.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Nov21():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-11-21.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-11-21.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Nov22():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-11-22.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-11-22.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Nov23():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-11-23.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-11-23.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Nov24():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-11-24.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-11-24.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Nov25():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-11-25.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-11-25.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Nov26():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-11-26.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-11-26.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Nov27():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-11-27.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-11-27.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Nov28():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-11-28.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-11-28.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Nov29():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-11-29.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-11-29.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+def Nov30():
+        workbook = Workbook("jasamargakodecabang2gerbang31_2023-11-30.json")
+        workbook.save("jasamargakodecabang2gerbang31_2023-11-30.sql")
+        print("Berhasil Convert Json ke SQL")
+        jpype.shutdownJVM()
+
+
+
+MenuToolsJsonSQL()
+
+pilih = int(input("Masukan nomor pilihan dalam menu:"))
+if pilih == 1:
+         Nov01()
+elif pilih==2:
+        Nov02()
+elif pilih==3:
+        Nov03()
+elif pilih==4:
+        Nov04()
+elif pilih==5:
+        Nov05()
+elif pilih==6:
+        Nov06()
+elif pilih==7:
+        Nov07()
+elif pilih==8:
+        Nov08()
+elif pilih==9:
+        Nov09()
+elif pilih==10:
+        Nov10()
+elif pilih==11:
+        Nov11()        
+elif pilih==12:
+        Nov12()
+
+elif pilih==13:
+        Nov13()
+elif pilih==14:
+        Nov14()
+elif pilih==15:
+        Nov15()
+elif pilih==16:
+        Nov16()
+elif pilih==17:
+        Nov17()
+elif pilih==18:
+        Nov18()
+elif pilih==19:
+        Nov20()
+elif pilih==20:
+        Nov21()
+elif pilih==21:
+        Nov22()
+elif pilih==22:
+        Nov23()
+elif pilih==23:
+        Nov24()
+elif pilih==24:
+        Nov25()
+elif pilih==25:
+        Nov26()
+elif pilih==26:
+        Nov27()
+elif pilih==27:
+        Nov28()
+elif pilih==28:
+        Nov29()
+elif pilih==29:
+        Nov29()
+elif pilih==30:
+        Nov30()
+
+        
+else:
+    print("Nomor menu tidak tersedia!")
+    exit
