aef98e0a704a69093a1a6f42a4b8d913ef27e838
AnandaRauf <anandaraufm@gmail.com>
2024-01-06 20:00:18 +0700

    Website Jasa Marga Update UI

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,15 @@
 module.exports = {
+<<<<<<< HEAD
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 }
+=======
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ]
+}
+>>>>>>> 50be8c0 (Website Jasa Marga Update UI)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 whitelist = ["gray", "red", "orange", "yellow", "green", "teal", "blue", "purple", "pink"].reduce(
@@ -24,3 +25,26 @@ module.exports = {
     require('@tailwindcss/aspect-ratio')
   ],
 };
+=======
+module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.js',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      zIndex: {
+        '-1': '-1',
+      },
+      flexGrow: {
+        '5' : '5'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+>>>>>>> 50be8c0 (Website Jasa Marga Update UI)
